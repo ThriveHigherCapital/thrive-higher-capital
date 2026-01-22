c0a836a55a0ee8329ab19b7b5e3e27cd3dfb8958
Alex Feleke <your-email@example.com>
2026-01-22 14:00:05 -0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/thrive-higher-capital/",
   plugins: [react()],
+  base: "/thrive-higher-capital/",
 });
 
